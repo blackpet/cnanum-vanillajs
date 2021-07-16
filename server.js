9245f219dd352c9119fba1048a8ef1d04dfd9e89
blcakpet <blackept@gmail.com>
2021-07-16 22:04:06 +0900

    BPF.Popup Skin 적용 (for firzzle)

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 4005;
+const port = process.env.PORT || 4003;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, 'assets')));
+
+app.post('*', (req, res) => {
+  res.redirect(req.originalUrl);
+});
 
 app.listen(port, () => {
   console.log(`server listen on ${port}`);
